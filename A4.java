7644e3de2ea730989eb682eb4386c8c8abd8b6de
Himani Raval <46948639+himani2100@users.noreply.github.com>
2019-03-12 14:42:28 -0400

    Add files via upload

--- A4.java
+++ A4.java
@@ -3,32 +3,21 @@ public class A4
 {
 static BufferedReader $br;
 static BufferedWriter $bw;
-static double f2( double x,  double y)throws Exception {
- double z;
+public static void main (String[] args) throws Exception{
+ int X;
 
-z= x*x-y*y;
-return z;
+X= 3;
+if( X == 3)
+{
+ int Y;
 
+} else {
+ String y= "test";
 
 }
-public static void main (String[] args) throws Exception{
- int x;
-
-$br = new BufferedReader(new FileReader("A41.input"));
-x = new Integer($br.readLine()).intValue();
-$br.close();
- int y;
-$br = new BufferedReader(new FileReader("A42.input"));
-y = new Integer($br.readLine()).intValue();
-$br.close();
- double z;
-z= f2(x, y)+f2(y, x)*0.5;
-$bw = new BufferedWriter(new FileWriter("A4.output"));
-$bw.write(""+ z);
+$bw = new BufferedWriter(new FileWriter("XKOUTPUT"));
+$bw.write(""+ (X+100));
 $bw.close();
-
-
-
 
 
 
